10ae9ae9522b6c22903942aa97f11ddd2432e0a4
TontonBanton <pancla13ulas@gmail.com>
2024-07-18 14:25:19 +0800

    Basic Component Layout

---
+++ src/components/Balance.vue
@@ -0,0 +1,4 @@
+<template>
+  <h4>Your Balance</h4>
+  <h1 id="balance">P0.00</h1>
+</template>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li class="minus">
+      Cash<span>P10,000.00</span>
+      <button class="delete-btn">x</button>
+    </li>
+    <li class="plus">
+      Dividend<span>P100,000.00</span>
+      <button class="delete-btn">x</button>
+    </li>
+  </ul>
+</template>
