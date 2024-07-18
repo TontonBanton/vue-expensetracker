a6e323a79db54357abc1ce2277778890d748ca16
TontonBanton <pancla13ulas@gmail.com>
2024-07-18 10:14:38 +0800

    Initial Start

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './assets/style.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+//Creates Vue application instance using the App component and mounts it to the element with the id="app" --> index.html <div id="app">
+createApp(App).mount('#app')
+
