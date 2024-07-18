4b7a4bb3dc828e3a91020468b8ee60e796afc768
TontonBanton <pancla13ulas@gmail.com>
2024-07-18 16:03:44 +0800

    Computed global balance through reduce

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <Header />
   <div class="container">
-    <Balance />
+    <Balance :total="total" />
     <IncomeExpenses />
     <TransactionList :transactions="transactions" />
     <AddTransaction />
@@ -15,11 +15,22 @@ import IncomeExpenses from './components/IncomeExpenses.vue'
 import TransactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
 
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 const transactions = ref([
-    {id: 1, text: 'Item1', amount: -19.99},
-    {id: 2, text: 'Salary', amount: 799.97},
+    {id: 1, text: 'Item1', amount: -119.99},
+    {id: 2, text: 'Salary', amount: 999.97},
     {id: 2, text: 'Item2', amount: -10},
     {id: 2, text: 'Sale', amount: 150}
 ])
+
+const total = computed(()=> {
+  const transactArray = transactions.value                            //No need if not beginner
+
+  return transactArray.reduce(                                       //Reduce method iterates over each element in transactArray.
+    //Reduce Callback function
+    (accumulator, transactArrayRow)=> {
+    return accumulator + transactArrayRow.amount                      //For each transactArrayRow, the amount property is added to the accumulator
+  }, 0)                                                               //Initial value of accumulator
+})
+
 </script>
