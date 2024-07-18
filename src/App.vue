1a25fa0dd0d3d1220205a0c03e33af96a983ff16
TontonBanton <pancla13ulas@gmail.com>
2024-07-18 16:54:05 +0800

    IncomeExpense Filter Reduce

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <Header />
   <div class="container">
     <Balance :total="total" />
-    <IncomeExpenses />
+    <IncomeExpenses :income="income" :expenses="expenses"/>
     <TransactionList :transactions="transactions" />
     <AddTransaction />
   </div>
@@ -16,6 +16,8 @@ import TransactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
 
 import { ref, computed } from 'vue'
+
+
 const transactions = ref([
     {id: 1, text: 'Item1', amount: -119.99},
     {id: 2, text: 'Salary', amount: 999.97},
@@ -23,14 +25,32 @@ const transactions = ref([
     {id: 2, text: 'Sale', amount: 150}
 ])
 
+//Get total
 const total = computed(()=> {
-  const transactArray = transactions.value                            //No need if not beginner
-
-  return transactArray.reduce(                                       //Reduce method iterates over each element in transactArray.
-    //Reduce Callback function
-    (accumulator, transactArrayRow)=> {
-    return accumulator + transactArrayRow.amount                      //For each transactArrayRow, the amount property is added to the accumulator
-  }, 0)                                                               //Initial value of accumulator
+  const transactArray = transactions.value              //Beginners only
+  return transactArray
+    .reduce((accumulator, transactArrayRow)=> {         //Iterates over each element in transactArray and accumulate the values(ex. amount) into a single value
+      return accumulator + transactArrayRow.amount      //For each transactArrayRow, the amount property is added to the accumulator
+    }, 0)                                               //Initial value of accumulator
 })
 
+//Get Income
+const income = computed(()=> {
+  return transactions.value
+    .filter((transactArrayRow)=> transactArrayRow.amount > 0)     //Add a filter for ammount greater than 0
+    .reduce((accumulator, transactArrayRow)=> {                   //Reduce filtered amount
+      return accumulator + transactArrayRow.amount
+    }, 0)
+    .toFixed(2)                                                  //Fomat result to two decimal places
+})
+
+//Get expenses
+const expenses = computed(()=> {
+  return transactions.value
+    .filter((transactArrayRow)=> transactArrayRow.amount < 0)     //Add a filter for ammount greater less than 0
+    .reduce((accumulator, transactArrayRow)=> {
+      return accumulator + transactArrayRow.amount
+    }, 0)
+    .toFixed(2)
+})
 </script>
